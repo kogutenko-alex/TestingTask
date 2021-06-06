b430cda51640d94759b5420b1025c8039ab2ca11
Aleksandr Kogutenko <oleksandr.kogutenko@gmail.com>
2021-06-06 18:12:50 +0300

    add comments

--- src/my/tasks/kogutenko/task1/TaskOne.java
+++ src/my/tasks/kogutenko/task1/TaskOne.java
@@ -3,7 +3,17 @@ package my.tasks.kogutenko.task1;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+/*
+   Task:
+       If we use a correctly written arithmetic expression that contains numbers,
+       transaction marks, and opening and closing parentheses, discard the numbers and transaction marks
+       and then write down the remaining parentheses without spaces between them,
+       We call the resulting result the correct bracket expression
+       [bracket expression "()(()))" is correct, and "()(" and "())(" is not].
 
+   Find the number of valid bracket expressions that contain N opening and N closing brackets.
+   N is entered from the keyboard. N is a non-negative integer.
+ */
 public class TaskOne {
 
     public static void main(String[] args) {
@@ -11,7 +21,10 @@ public class TaskOne {
         int N;
         boolean circle = true;
         String answer;
+        //create the loop for make several counts
         while(circle) {
+            //loop for checking correct input value
+            //while entering incorrectly that loop continues
             while (true) {
                 System.out.print("Enter the amount of numbers: ");
                 N = scanner.nextInt();
@@ -23,6 +36,8 @@ public class TaskOne {
                 }
             }
             System.out.println("Answer is " + countBrackets(N));
+            //a loop to check that the answer to a question is correct.
+            // check by regular expression
             while (true) {
                 System.out.print("Do you want to continue? (1 - yes; 0 - no)\n>>> ");
                 Pattern pattern = Pattern.compile("[0-1]{1}");
--- src/my/tasks/kogutenko/task2/TaskTwo.java
+++ src/my/tasks/kogutenko/task2/TaskTwo.java
@@ -1,7 +1,15 @@
 package my.tasks.kogutenko.task2;
 
+import my.tasks.kogutenko.task2.data.Reader;
+
 public class TaskTwo {
     public static void main(String[] args) {
-
+        //new TestData(); //uncommented the line if you want use the test input data
+        //commented next lines if you want use the test input data
+        Reader reader = new Reader();
+        for (String key : reader.getRunPaths().keySet()) {
+            reader.getGraph().shortestPath(key);
+            reader.getGraph().printCost(reader.getRunPaths().get(key));
+        }
     }
 }
